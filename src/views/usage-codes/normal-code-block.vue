227aca19da4a3c872ac65c9649d8bed81daeb31c
Saran Tanpituckpong <sarunta@gmail.com>
2017-05-28 18:12:15 +0700

    Add indent

--- src/views/usage-codes/normal-code-block.vue
+++ src/views/usage-codes/normal-code-block.vue
@@ -1,12 +1,14 @@
 <template>
 	<div>
-		<highlight-code lang="javascript">var map;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	});
-}</highlight-code>
+		<highlight-code lang="javascript">
+			var map;
+			function initMap() {
+				map = new google.maps.Map(document.getElementById('map'), {
+					center: {lat: -34.397, lng: 150.644},
+					zoom: 8
+				});
+			}
+		</highlight-code>
 	</div>
 </template>
 
