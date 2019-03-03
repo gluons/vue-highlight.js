26531008cfef9ef5aa1d6889e492e698a9bd3870
Saran Tanpituckpong <sarunta@gmail.com>
2019-03-03 14:42:46 +0700

    :memo: Update usage

--- src/views/home/Usage.vue
+++ src/views/home/Usage.vue
@@ -7,16 +7,28 @@
 		import VueHighlightJS from 'vue-highlight.js';
 		import App from './App';
 
-		/*
-		 * Use Vue Highlight.js
-		 */
-		Vue.use(VueHighlightJS);
+		// Highlight.js languages (Only required languages)
+		import css from 'highlight.js/lib/languages/css';
+		import javascript from 'highlight.js/lib/languages/javascript';
+		import vue from 'vue-highlight.js/lib/languages/vue';
 
 		/*
 		 * Import Highlight.js theme
 		 * Find more: https://highlightjs.org/static/demo/
 		 */
 		import 'highlight.js/styles/default.css';
+
+		/*
+		 * Use Vue Highlight.js
+		 */
+		Vue.use(VueHighlightJS, {
+			// Register only languages that you want
+			languages: {
+				css,
+				javascript,
+				vue
+			}
+		});
 
 		new Vue({
 			el: '#app',
