84f8bffc0c34a8e7e74849e2b246bb468c5c6823
Saran Tanpituckpong <sarunta@gmail.com>
2021-02-28 17:17:48 +0700

    ♻️ Move code block to data

--- src/views/home/Usage.vue
+++ src/views/home/Usage.vue
@@ -2,44 +2,48 @@
 #usage
 	h2 Usage
 	h3 Main File:
-	highlight-code(lang='javascript').
-		import Vue from 'vue';
-		import VueHighlightJS from 'vue-highlight.js';
-		import App from './App';
-
-		// Highlight.js languages (Only required languages)
-		import css from 'highlight.js/lib/languages/css';
-		import javascript from 'highlight.js/lib/languages/javascript';
-		import vue from 'vue-highlight.js/lib/languages/vue';
-
-		/*
-		 * Import Highlight.js theme
-		 * Find more: https://highlightjs.org/static/demo/
-		 */
-		import 'highlight.js/styles/default.css';
-
-		/*
-		 * Use Vue Highlight.js
-		 */
-		Vue.use(VueHighlightJS, {
-			// Register only languages that you want
-			languages: {
-				css,
-				javascript,
-				vue
-			}
-		});
-
-		new Vue({
-			el: '#app',
-			render: h => h(App)
-		});
+	highlight-code(lang='javascript' :code='mainFileContent')
 	h3 Vue File:
-	highlight-code(lang='vue', :code='vueFileContent')
+	highlight-code(lang='vue' :code='vueFileContent')
 </template>
 
 <script>
 /* eslint-disable no-useless-escape */
+
+const mainFileContent = `
+import Vue from 'vue';
+import VueHighlightJS from 'vue-highlight.js';
+import App from './App';
+
+// Highlight.js languages (Only required languages)
+import css from 'highlight.js/lib/languages/css';
+import javascript from 'highlight.js/lib/languages/javascript';
+import vue from 'vue-highlight.js/lib/languages/vue';
+
+/*
+ * Import Highlight.js theme
+ * Find more: https://highlightjs.org/static/demo/
+ */
+import 'highlight.js/styles/default.css';
+
+/*
+ * Use Vue Highlight.js
+ */
+Vue.use(VueHighlightJS, {
+	// Register only languages that you want
+	languages: {
+		css,
+		javascript,
+		vue
+	}
+});
+
+new Vue({
+	el: '#app',
+	render: h => h(App)
+});
+`;
+
 const vueFileContent = `
 <template>
 	<div id="app">
@@ -68,6 +72,7 @@ export default {
 	name: 'Usage',
 	data() {
 		return {
+			mainFileContent,
 			vueFileContent
 		};
 	}
